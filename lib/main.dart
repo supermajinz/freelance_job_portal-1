3367dfba41bbcc420bb7db4fb0ca6976d8137aba
Z <zen.makhlouf@gmail.com>
2024-06-24 16:36:34 -0700

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freelance_job_portal/core/localization/bloc/localization_bloc.dart';
 import 'package:freelance_job_portal/core/utils/app_router.dart';
+import 'package:freelance_job_portal/l10n/l10n.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'l10n/l10n.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  runApp(const FreelanceJob());
+  final sharedPreferences = await SharedPreferences.getInstance;
+  runApp(BlocProvider(
+    create: (context) => LocalizationBloc()..add(LoadSavedLocalization()),
+    child: const FreelanceJob(),
+  ));
 }
-//zen
 
-//ahmad
-//code bad
 class FreelanceJob extends StatelessWidget {
   const FreelanceJob({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-);
+    return BlocBuilder<LocalizationBloc, LocalizationState>(
+      builder: (context, state) {
+        return MaterialApp.router(
+          supportedLocales: L10n.all,
+          locale: state.locale,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
   }
 }
