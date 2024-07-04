bc3b7f477f5e2c44989a6626dce4caa675ca65f9
ahmad <ahmad@gmail.com>
2024-07-04 05:10:19 -0700

    4.7.2024

--- lib/features/onboarding/presentation/views/widget/onboarding_body.dart
+++ lib/features/onboarding/presentation/views/widget/onboarding_body.dart
@@ -68,7 +68,7 @@ class _OnboardingBodyState extends State<OnboardingBody> {
             left: SizeConfig.defaultSize! * 8,
             child: CustomButtonGeneral(
               onPressed: () {
-                GoRouter.of(context).push("/home");
+                GoRouter.of(context).push("/homescreen");
               },
               width: SizeConfig.defaultSize! * 25.5,
               borderSide: const BorderSide(width: 1),
